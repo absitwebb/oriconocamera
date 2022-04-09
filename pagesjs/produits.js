ee6b39c75ec74dd0924d65b0a4230e86b51751ac
absitwebb <absitwebb@gmail.com>
2022-04-09 18:20:59 +0200

    page produits 09/04/22 19H

--- pagesjs/produits.js
+++ pagesjs/produits.js
@@ -16,3 +16,38 @@ function AffichTextBanniere(TextBanniere) {
 //affiche img et text dans bannière vintage
 affichImgBanniere(ImgbanniereVintage);
 AffichTextBanniere("Vintage");
+
+//requete flex produits
+let ProduitsData = [];
+//récupère data dans lien flex
+const RecupProduits = async () => {
+  await fetch("http://localhost:3000/api/cameras")
+    .then((res) => res.json())
+    .then((data) => {
+      ProduitsData = data;
+      console.log(ProduitsData);
+    });
+};
+// récupère et affiche chaque produit
+const ProduitsLists = async () => {
+  await RecupProduits();
+  document.querySelector(".affichProduits").innerHTML = ProduitsData.map(
+    (Produit) => `
+  <div id="cam ${Produit._id}" class="bloc-produit">
+   <h3 class="bloc-produit_title">${Produit.name}</h3>
+   <img class="bloc-produit_img" src="${Produit.imageUrl}" alt="image produit ${
+      Produit.name
+    }"/> 
+   <p class="bloc-produit_descript"> ${Produit.description}</p>
+   <p class="bloc-produit_price">${Produit.price
+     .toString()
+     .replace(/0+$/, "")} Euros </p>
+     <button id="${
+       Produit._id
+     }" class="bloc-produit_buttonenvoie"> voir Le produit</button>
+  </div>
+  `
+    // join pour supprimer virgule entre chaque affichage de produit
+  ).join("");
+};
+ProduitsLists();
