8919bd344e336fc041e4cef948c75b3dcdbae12a
absitwebb <absitwebb@gmail.com>
2022-04-09 08:23:01 +0200

    page produits 09/04/22

---
+++ pagesjs/produits.js
@@ -0,0 +1,18 @@
+//variable img bannière
+let ImgbanniereVintage = "public/img/camera-vintage.png";
+
+//fonction pour afficher img dans bannière
+function affichImgBanniere(imgBanniere) {
+  let boxImgBanniere = document.createElement("img");
+  boxImgBanniere.src = imgBanniere;
+  document.querySelector(".bannierename").appendChild(boxImgBanniere);
+}
+
+function AffichTextBanniere(TextBanniere) {
+  let boxTextBanniere = document.createElement("h1");
+  boxTextBanniere.innerHTML = ` ${TextBanniere}`;
+  document.querySelector(".bannierename").appendChild(boxTextBanniere);
+}
+//affiche img dans bannière vintage
+affichImgBanniere(ImgbanniereVintage);
+AffichTextBanniere("Vintage");
