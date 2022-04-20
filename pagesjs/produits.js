b6acaaa82ffd07775987f87d510120446473b40c
absitwebb <absitwebb@gmail.com>
2022-04-20 14:24:50 +0200

    panier 20/04/22 14h10

--- pagesjs/produits.js
+++ pagesjs/produits.js
@@ -42,30 +42,5 @@ const ProduitsLists = async () => {
 ProduitsLists();
 
 //-----------afficher quantité panier dans bannière------------
-// on va chercher les produits dans le localstorage
-function ajoutpanierQauntiteTotal() {
-  let produitLocal = JSON.parse(localStorage.getItem("produit"));
-  // on crée un tableau pour stocké la quantité de produits qu'on va trouver
-  let appareilQauntiteTotal = [];
 
-  // on fait une boucle pour chercher la quantité de produits
-  // on met cette quantié dans un tableau
-  if (produitLocal) {
-    produitLocal.forEach((appareil) => {
-      appareilQauntiteTotal.push(appareil.quantite);
-      // on supprimer la class panierbannierevide
-      // on ajout ou modifie la class de id panierbannierequantite
-      panierBanniereQuantite.classList.remove("panierbannierevide");
-      panierBanniereQuantite.classList.add("panierbannierequantite");
-      // on affiche le resultat du tableau dans la page
-      // eval et le + dans join permet d'additionner le tableau
-      panierBanniereQuantite.textContent = `${eval(
-        appareilQauntiteTotal.join("+")
-      )}`;
-    });
-  } else {
-    panierBanniereQuantite.classList.remove("panierbannierequantite");
-    panierBanniereQuantite.classList.add("panierbannierevide");
-  }
-}
 ajoutpanierQauntiteTotal();
